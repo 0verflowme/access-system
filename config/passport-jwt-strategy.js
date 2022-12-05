6a0a6c403941ad1cd152b457b1db2527e2522943
0verflowme <priyanshukumarpu@gmail.com>
2022-12-05 14:34:33 +0000

    ref

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -41,7 +41,7 @@ passport.checkAuthentication = function (req, res, next) {
 	if (req.isAuthenticated()) {
 		return next();
 	}
-	return res.redirect("/signin");
+	return res.redirect("/login");
 };
 
 passport.checkAuthenticationSignin = function (req, res, next) {
